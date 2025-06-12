9d85a7292249f899a214ff32fed0b40d735f49ec
pguevara135 <pguevara135@hotmail.com>
2025-06-12 15:28:49 -0300

    REmanufacturated fechar btn

--- src/JS/script.js
+++ src/JS/script.js
@@ -2,15 +2,35 @@ setTimeout(function() {
     location.reload();
 }, 12000000); // Refresh every 20 minutes
 
+// Carregamento da Pagina
+// Quando a página carregar
+    window.onload = function () {
+      // Verifica se já foi exibido
+      const avisoExibido = localStorage.getItem('cookieAvisoExibido');
 
-// Functio para fechar o banner de cookies
-function fecharBanner() {
-      document.getElementById('cookieBanner').style.display = 'none';
-      localStorage.setItem('cookieAvisoExibido',  'true');
-    };
-
-window.onload = function() {
-      if (!localStorage.getItem('cookieAvisoExibido')) {
+      // Se ainda não foi exibido, mostra o aviso
+      if (!avisoExibido) {
         document.getElementById('cookieBanner').style.display = 'block';
       }
+
+      // Adiciona evento ao botão
+      document.getElementById('fechar-btn').onclick = function () {
+        // Esconde o aviso
+        document.getElementById('cookieBanner').style.display = 'none';
+        // Salva informação no localStorage
+        localStorage.setItem('cookieAvisoExibido', 'sim');
+      };
     };
+
+
+
+// function fecharBanner() {
+//       document.getElementById('cookieBanner').style.display = 'none';
+//       localStorage.setItem('cookieAvisoExibido',  'true');
+//     };
+
+// window.onload = function() {
+//       if (!localStorage.getItem('cookieAvisoExibido')) {
+//         document.getElementById('cookieBanner').style.display = 'block';
+//       }
+//     };
