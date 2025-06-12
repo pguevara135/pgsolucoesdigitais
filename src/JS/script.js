02985ce4ffee7e95bd6a1438a6a9e8d5d54f8121
pguevara135 <pguevara135@hotmail.com>
2025-06-12 16:01:00 -0300

    insert display in banner

--- src/JS/script.js
+++ src/JS/script.js
@@ -4,23 +4,23 @@ setTimeout(function() {
 
 // Carregamento da Pagina
 // Quando a página carregar
-    window.onload = function () {
-      // Verifica se já foi exibido
-      const avisoExibido = localStorage.getItem('cookieAvisoExibido');
+window.onload = function () {
+  // Verifica se já foi exibido
+  const avisoExibido = localStorage.getItem('cookieAvisoExibido');
 
-      // Se ainda não foi exibido, mostra o aviso
-      if (!avisoExibido) {
-        document.getElementById('cookieBanner').style.display = 'block';
-      }
+  // Se ainda não foi exibido, mostra o aviso
+  if (!avisoExibido) {
+    document.getElementById('cookieBanner').style.display = 'block';
+  }
 
-      // Adiciona evento ao botão
-      document.getElementById('fechar-btn').onclick = function () {
-        // Esconde o aviso
-        document.getElementById('cookieBanner').style.display = 'none';
-        // Salva informação no localStorage
-        localStorage.setItem('cookieAvisoExibido', 'sim');
-      };
-    };
+  // Adiciona evento ao botão
+  document.getElementById('fechar-btn').onclick = function () {
+    // Esconde o aviso
+    document.getElementById('cookieBanner').style.display = 'none';
+    // Salva informação no localStorage
+    localStorage.setItem('cookieAvisoExibido', 'sim');
+  };
+};
 
 
 
