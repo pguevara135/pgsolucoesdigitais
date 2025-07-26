5132b79f4591fce82e9b24d7e74ccbabc23f7d76
pguevara135 <pguevara135@hotmail.com>
2025-07-25 21:42:59 -0300

    create contato.css

--- src/JS/script.js
+++ src/JS/script.js
@@ -2,18 +2,19 @@ setTimeout(function() {
     location.reload();
 }, 12000000); // Refresh every 20 minutes
 
-// Carregamento da Pagina
-// Quando a página carregar
+//? Carregamento da Pagina
+
 window.onload = function () {
   // Verifica se já foi exibido
   const avisoExibido = localStorage.getItem('cookieAvisoExibido');
 
-  // Se ainda não foi exibido, mostra o aviso
+  //? Se ainda não foi exibido, mostra o aviso
   if (!avisoExibido) {
     document.getElementById('cookieBanner').style.display = 'block';
   }
 
-  // Adiciona evento ao botão
+  //! Adiciona evento ao botão
+
   document.getElementById('fechar-btn').onclick = function () {
     // Esconde o aviso
     document.getElementById('cookieBanner').style.display = 'none';
@@ -21,3 +22,18 @@ window.onload = function () {
     localStorage.setItem('cookieAvisoExibido', 'sim');
   };
 };
+
+//! Envio do Formulário de Contato
+
+document.getElementById("contato-form").addEventListener("submit", async function(event) {
+  event.preventDefault(); // Previne o envio padrão do formulário
+
+  const form = event.target;
+  const dados = new FormData(form);
+
+  const resposta = await fetch.text();
+  document.getElementById("resultado").innerHTML = texto;
+
+  formm.reset(); // Limpa o formulário após o envio
+
+});
